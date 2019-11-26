d417356bbd917ae9e6f07d0ec4059eb13ccbf9e2
alrogozin <alrogozin@gmail.com>
2019-11-26 16:36:47 +0300

    26112019

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,65 @@
 <template>
   <div id="app">
+
+
+    <!-- https://itchief.ru/lessons/bootstrap-3/38-bootstrap-3-navigation-menu -->
+    <!-- Классы navbar и navbar-default (базовые классы меню) -->
+    <!-- <nav class="navbar-static-top | navbar-fixed-top"... - определяют поведение fixed - часть с меню всегда на экране при прокрутке странице -->
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <!-- Контейнер (определяет ширину Navbar) -->
+            <div class="container-fluid">
+                <!-- Заголовок -->
+                <div class="navbar-header">
+                    <!-- Кнопка «Гамбургер» отображается только в мобильном виде (предназначена для открытия основного содержимого Navbar) -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-main">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!-- Бренд или название сайта (отображается в левой части меню) -->
+                    <!-- <a class="navbar-brand"> -->
+                        <!-- <img src="navbar-brand.svg" height="36" alt="Brand name"> -->
+                        <router-link class="navbar-brand" to='/'>Brand name</router-link>
+                    <!-- </a> -->
+                </div>
+                <!-- Основная часть меню (может содержать ссылки, формы и другие элементы) -->
+                <div class="collapse navbar-collapse" id="navbar-main">
+                    <!-- Содержимое основной части -->
+                    <ul class="nav navbar-nav">
+                        <!-- <li class="active"><a href="#">Страница 1</a></li> -->
+                        <li><router-link to='/'>Home</router-link></li>
+                        <li><router-link to='/register'>Register</router-link></li>
+                        <li><router-link to='/login'>Login</router-link></li>
+                        <!-- <li><a href="#">Страница 3</a></li>
+                        <li><a href="#">Страница 4</a></li>
+                        <li><a href="#">Страница 5</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Раздел <span
+                                    class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Ссылка #1</a></li>
+                                <li><a href="#">Ссылка #2</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">Ссылка #3</a></li>
+                                <li><a href="#">Ссылка #4</a></li>
+                            </ul>
+                        </li> -->
+                    </ul>
+                </div>
+            </div>
+        </nav>
+<!-- 
 	<nav>
 		<router-link to='/'>Home&nbsp;</router-link>
 		<router-link to='/register'>Register&nbsp;</router-link>
 		<router-link to='/login'>Login&nbsp;</router-link>
-	</nav>
+	</nav> -->
 	<router-view/>
 <!--     <img alt="Vue logo" src="./assets/logo.png"> -->
   </div>
 </template>
 
 <script>
-
-/* import HelloWorld from './components/HelloWorld.vue' */
 
 export default {
   name: 'app',
@@ -23,11 +70,13 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Avenir Next Cyr Medium', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: 60px;
+  margin-right: 60px;
 }
 </style>
--- src/components/register.vue
+++ src/components/register.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         Register component
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia a, numquam impedit incidunt ut distinctio eligendi dolorum inventore dolorem blanditiis provident, rerum reiciendis non tempore voluptas sint. Incidunt vero minima ipsum veritatis optio placeat iure, odit sint voluptates provident est eum dolorem magni vel recusandae enim natus repellat omnis, aperiam quas quae facere dolore labore quaerat. Velit ipsa amet voluptatibus, expedita et voluptatum rerum nobis quibusdam, sapiente hic laudantium quia exercitationem ipsum earum laborum officia cumque non fugit explicabo quis repudiandae, aliquid provident libero deserunt. Perspiciatis obcaecati voluptates itaque, minus molestiae expedita, facere similique enim dignissimos assumenda quibusdam quas tempore.</p>
     </div>
 </template>
 <script>
